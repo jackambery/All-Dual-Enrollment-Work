dfdfe90f330060d96f870f196667aa20dc342fe6
Jack Ambery <john.ambery@bishopireton.org>
2020-09-17 21:44:51 -0400

    testing with BigInteger

--- BigInteger/src/BigInteger.java
+++ BigInteger/src/BigInteger.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
+
+/** 
+ * @author Jack Ambery
+ * 
+ *
+ */
 
 public class BigInteger {
-
+	
+	public String fibonacci(int n) {
+		
+		ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+		int temp = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			temp += n;
+			String tempString = Integer.toString(temp);
+			
+			list.add(new BigInteger(tempString));
+		}
+		
+		return list.toString();
+	}
 }
