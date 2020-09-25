b2c91f1263fb64ef38f61795e4f7a28db873106f
Jack Ambery <john.ambery@bishopireton.org>
2020-09-24 21:38:09 -0400

    Does not get flagged with errors now

--- BigInteger/src/BigInteger.java
+++ BigInteger/src/BigInteger.java
@@ -11,13 +11,13 @@ public class BigInteger {
 	public String fibonacci(int n) {
 		
 		ArrayList<BigInteger> list = new ArrayList<BigInteger>();
-		int temp = 0;
+		//int temp = 0;
 		
 		for (int i = 1; i <= n; i++) {
-			temp += n;
-			String tempString = Integer.toString(temp);
+			//temp += n;
+			//String tempString = Integer.toString(temp);
 			
-			list.add(new BigInteger(tempString));
+			//list.add(new BigInteger(tempString));
 		}
 		
 		return list.toString();
