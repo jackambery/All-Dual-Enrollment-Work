9494e14dc8aa18700ede5f84a1641976ef6eeb96
Jack Ambery <john.ambery@bishopireton.org>
2020-09-24 22:25:09 -0400

    Program works as it should!

--- MatchingBraces/MatchingBraces/src/main/MatchingBraces.java
+++ MatchingBraces/MatchingBraces/src/main/MatchingBraces.java
@@ -3,13 +3,25 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Jack Ambery
+ * <p>This class takes in a document and tests if there is an equal 
+ * amount of open and closed braces.
+ * </p>
+ *
+ */
+
 public class MatchingBraces {
 	
-	
+	/**
+	 * @param file the file user wants to test
+	 * @return true if bracket issues are not evident, false otherwise
+	 */
 	public static boolean bracketCheck(File file) {
 		
-		Scanner input = null;
-		int count = 0;
+		Scanner input = null;    //initialized if file exists
+		int count = 0;    //should end at zero if file is good
 		
 		try {
 			input = new Scanner(file);
@@ -20,8 +32,9 @@ public class MatchingBraces {
 			System.exit(1);
 		}
 		
-		for (String arr : file.list()) {
-			for (char letter : arr.toCharArray()) {
+		while (input.hasNextLine()) {
+			String line = input.nextLine();
+			for (char letter : line.toCharArray()) {
 				if (letter == '{') {
 					count++;
 				}
@@ -31,17 +44,6 @@ public class MatchingBraces {
 			}
 		}
 		
-		/*
-		while (count <= file.length()) {
-			if (input.next().contentEquals("{")) {
-				count++;
-			}
-			if (input.next().contentEquals("}")) {
-				count--;
-			}
-		}
-		*/
-		
 		if (count == 0) {
 			return true;
 		}
@@ -50,10 +52,15 @@ public class MatchingBraces {
 		
 	}
 	
+	/**
+	 * This is the test class.
+	 * 
+	 * @param args
+	 */
 	public static void main(String args[]) {
 		
-		//String pathname = "testfile";
-		File file = new File("testfile");
+		String pathname = "testfile";
+		File file = new File(pathname);
 		
 		if (bracketCheck(file) == true) {
 			System.out.println(file.getName() + " has no bracket issues.");
