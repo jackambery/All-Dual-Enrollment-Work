fdd1526d81d4fabe3c5f551878d7b928043947b1
Jack Ambery <john.ambery@bishopireton.org>
2020-09-26 15:29:39 -0400

    Added pieces and main method

--- Chessboard/src/Chessboard.java
+++ Chessboard/src/Chessboard.java
@@ -1,3 +1,7 @@
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  * 
  */
@@ -6,10 +10,33 @@
  * @author Jack Ambery
  *
  */
-public class Chessboard {
+public class Chessboard extends JPanel {
 	
 	public Chessboard() {
 		
+	    JFrame window = createFrame();
+	    createPanel();
+	    window.add(this);
+	    window.pack();
+	    window.setVisible(true);
+	}
+	
+	private JFrame createFrame() {
+		
+		JFrame frame = new JFrame("Chessboard");
+		frame.setSize(400, 700);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		return frame;
+	} 
+	
+	private void createPanel() {
+		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		//add(new Display());
+		//add(new Keypad());
+	}
+	
+	public static void main(String[] args) {
+		Chessboard c = new Chessboard();
 	}
 
 }
