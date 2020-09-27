8b54cdd00ed5c7e4f50d8ce79b880a920668ab74
Jack Ambery <john.ambery@bishopireton.org>
2020-09-26 21:24:46 -0400

    created drag-able pieces

--- Chessboard/src/Chessboard.java
+++ Chessboard/src/Chessboard.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 
@@ -17,19 +18,19 @@ import javax.swing.JPanel;
 public class Chessboard extends JPanel {
 	
 	//Constants
-	private final static String WHITE_PAWN = "\u2659 ";
-	private final static String WHITE_KING = "\u2654 ";
-	private final static String WHITE_QUEEN = "\u2655 ";
-	private final static String WHITE_BISHOP = "\u2657 ";
-	private final static String WHITE_KNIGHT = "\u2658 ";
-	private final static String WHITE_ROOK = "\u2656 ";
+	private final static String WHITE_PAWN = "\u2659";
+	private final static String WHITE_KING = "\u2654";
+	private final static String WHITE_QUEEN = "\u2655";
+	private final static String WHITE_BISHOP = "\u2657";
+	private final static String WHITE_KNIGHT = "\u2658";
+	private final static String WHITE_ROOK = "\u2656";
 	
-	private final static String BLACK_PAWN = "\u265F ";
-	private final static String BLACK_KING = "\u265A ";
-	private final static String BLACK_QUEEN = "\u265B ";
-	private final static String BLACK_BISHOP = "\u265D ";
-	private final static String BLACK_KNIGHT = "\u265E ";
-	private final static String BLACK_ROOK = "\u265C ";
+	private final static String BLACK_PAWN = "\u265F";
+	private final static String BLACK_KING = "\u265A";
+	private final static String BLACK_QUEEN = "\u265B";
+	private final static String BLACK_BISHOP = "\u265D";
+	private final static String BLACK_KNIGHT = "\u265E";
+	private final static String BLACK_ROOK = "\u265C";
 
 	
 	JPanel squares[][] = new JPanel[8][8];
@@ -70,21 +71,44 @@ public class Chessboard extends JPanel {
 		}
 	}
 	
-	/*
 	public void addPieces() {
 		setLayout(new GridLayout(8, 8));
+    	DragListener drag = new DragListener();
 		
 		for (int i = 0; i < 8; i++) {
 	        for (int j = 0; j < 8; j++) {
-	            pieces[i][j] = new JButton("test");
-	            pieces[i][j].setSize(75, 75);
-	            pieces[i][j].setText(BLACK_BISHOP);
-	            pieces[i][j].setVisible(true);
-	            add(pieces[i][j]);
+	            pieces[i][j] = new JButton("");
+	        	squares[i][j].add(pieces[i][j]);
+	        	pieces[i][j].addMouseListener(drag);
+	        	pieces[i][j].addMouseMotionListener(drag);
+	            	        
 	        }
 		}
+		
+		for (int i = 0; i < 8; i++) {
+			pieces[1][i].setText(BLACK_PAWN);
+			pieces[6][i].setText(WHITE_PAWN);
+		}
+		
+        pieces[0][0].setText(BLACK_ROOK);
+        pieces[0][1].setText(BLACK_KNIGHT);
+        pieces[0][2].setText(BLACK_BISHOP);
+        pieces[0][3].setText(BLACK_QUEEN);
+        pieces[0][4].setText(BLACK_KING);
+        pieces[0][5].setText(BLACK_BISHOP);
+        pieces[0][6].setText(BLACK_KNIGHT);
+        pieces[0][7].setText(BLACK_ROOK);
+
+        pieces[7][0].setText(WHITE_ROOK);
+        pieces[7][1].setText(WHITE_KNIGHT);
+        pieces[7][2].setText(WHITE_BISHOP);
+        pieces[7][3].setText(WHITE_KING);
+        pieces[7][4].setText(WHITE_QUEEN);
+        pieces[7][5].setText(WHITE_BISHOP);
+        pieces[7][6].setText(WHITE_KNIGHT);
+        pieces[7][7].setText(WHITE_ROOK);
+
 	}
-	*/
 	
 	
 	public static void main(String[] args) {
