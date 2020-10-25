1dae3fa9196ff5ae275e970b8e8e0d37e7467851
Jack Ambery <john.ambery@bishopireton.org>
2020-10-24 23:01:16 -0400

    IndexEntry done

---
+++ IndexMaker/src/IndexEntry.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class IndexEntry {
+	
+	private String word;
+	private TreeSet<Integer> lines;
+	
+	public IndexEntry(String word) {
+		this.word = word;
+		lines = new TreeSet<Integer>();
+	}
+	
+	public void add(int num) { 
+		if(!(lines.contains(num))) {
+			lines.add(num);			
+		}
+	}
+	
+	public String getWord() {
+		return word;
+	}
+	
+	public String toString() {
+		Iterator<Integer> iterator = lines.iterator();
+		String str = "";
+		while (iterator.hasNext()) {
+			str += word + iterator.next() + " ";
+		}
+		return str;
+	}
+
+}
