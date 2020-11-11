c2ca2249046a90563071868c1027065ce337563b
Jack Ambery <john.ambery@bishopireton.org>
2020-11-11 09:19:04 -0500

    first commit, set up BrowserModel

---
+++ Ch22BrowserLab/src/BrowserModel.java
@@ -0,0 +1,31 @@
+
+public class BrowserModel {
+	
+	public BrowserModel(BrowserView view) {
+		
+	}
+	
+	public void back() {
+		
+	}
+	
+	public void forward() {
+		
+	}
+
+	public void home() {
+		
+	}
+	
+	public void followLink(int n) {
+		
+	}
+	
+	public boolean hasBack() {
+		return false;
+	}
+	
+	public boolean hasForward() {
+		return false;
+	}
+}
