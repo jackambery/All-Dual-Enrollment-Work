1878ea6eaec1ee84ca19ea86a16d0db827a5cb1b
Jack Ambery <john.ambery@bishopireton.org>
2020-11-13 10:21:06 -0500

    tower done for now

--- ProductionLine/src/Tower.java
+++ ProductionLine/src/Tower.java
@@ -1,4 +1,16 @@
+import java.util.Stack;
 
 public class Tower {
+	
+	private Stack<Disk> tower;
+	
+	public Tower() {
+		this.tower = new Stack<Disk>();
+	}
+	
+	//just use push() instead of addDisk()
+//	public void addDisk(Disk disk) {
+//		tower.push(disk);
+//	}
 
 }
