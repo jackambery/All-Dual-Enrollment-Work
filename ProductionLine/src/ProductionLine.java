11af3e5d89fe647de3184a09e8c06c0b03a989a0
Jack Ambery <john.ambery@bishopireton.org>
2020-11-13 15:22:18 -0500

    work on ProductionLine

--- ProductionLine/src/ProductionLine.java
+++ ProductionLine/src/ProductionLine.java
@@ -1,4 +1,35 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class ProductionLine {
+	
+	private Queue<Disk> input;
+	private Queue<Tower> output;
+	private Stack<Disk> robotArm;
+	
+	public ProductionLine() {
+		input = new LinkedList<Disk>();
+		output = new LinkedList<Tower>();
+		robotArm = new Stack<Disk>();
+	}
+	
+	public void addDisk(Disk disk) {
+		input.add(disk);
+	}
+	
+	public void unloadRobot() {
+		Tower pyramid = new Tower(); //type Stack
+		while (robotArm.size() > 0) {
+			pyramid.addDisk(robotArm.pop());
+		}
+		output.add(pyramid);
+	}
+	
+	
+	//**********Main Method************
+	public static void main(String[] args) {
+		
+	}
 
 }
--- ProductionLine/src/Tower.java
+++ ProductionLine/src/Tower.java
@@ -3,14 +3,17 @@ import java.util.Stack;
 public class Tower {
 	
 	private Stack<Disk> tower;
+	private int size;
 	
 	public Tower() {
 		this.tower = new Stack<Disk>();
+		size = 0;
 	}
 	
 	//just use push() instead of addDisk()
-//	public void addDisk(Disk disk) {
-//		tower.push(disk);
-//	}
+	public void addDisk(Disk disk) {
+		tower.push(disk);
+		size++;
+	}
 
 }
