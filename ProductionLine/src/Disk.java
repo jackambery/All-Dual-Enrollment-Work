0bbfc78f4ff9c30ebba2ae41177fc1556996564d
Jack Ambery <john.ambery@bishopireton.org>
2020-11-13 09:30:40 -0500

    Disk done

--- ProductionLine/src/Disk.java
+++ ProductionLine/src/Disk.java
@@ -1,4 +1,25 @@
 
-public class Disk {
+public class Disk implements Comparable<Disk>{
+	
+	private int radius;
+	
+	public Disk(int radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public int compareTo(Disk disk) {
+		if (this.radius > disk.radius) {
+			return 1;
+		}
+		if (this.radius == disk.radius) {
+			return 0;
+		}
+		return -1;
+	}
+	
+	public int getRadius() {
+		return radius;
+	}
 
 }
