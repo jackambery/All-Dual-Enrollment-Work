000fb406d5ff991f32e88bbf052614441dbe0c34
Jack Ambery <john.ambery@bishopireton.org>
2020-12-24 22:16:36 -0500

    More tests, input all set

--- Knapsack/src/Knapsack.java
+++ Knapsack/src/Knapsack.java
@@ -1,17 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Knapsack {
-	
-	//fields
-	//private static int[] w;
-	//private static int n = w.length;
-	//private static int limit;
-	
-	//private static List<Integer> list;
 
 	public static int max(int i, int k) {
 		if (i > k) {
@@ -44,76 +38,46 @@ public class Knapsack {
 		return max(total1, total2);
 	}
 	
-	public static int knapsackSum(int[] w, int n, int limit, List<Integer> list) {
+	
+//	
+	//	
+	public static int knapsackSum(int[] w, int n, int limit, ArrayList<Integer> list) {
 		int total1 = 0;
 		int total2 = 0;
 		int i = 0;
 		List<Integer> listA = new ArrayList<Integer>();
 		List<Integer> listB = new ArrayList<Integer>();
-		while (i < w.length) {
-			if (n < 0 || limit == 0) {
-				return 0;
-			}
-			if (w[i] >= limit) {
-				listA.add(w[i]);
-				total1 = knapsackSum(w, n - 1, limit, list);
-			}
-			else {
-				listA.clear();
-				total1 = 0;
-			}
-			if (w[i] <= limit) {
-				listB.add(w[i]);
-				total2 = knapsackSum(w, n - 1, limit - w[i], list) + w[i];
-			}
-			else {
-				listB.clear();
-				total2 = 0;
-			}
-			i++;
+		if (n < 0 || limit == 0) {
+			return 0;
 		}
-		
+		if (w[n] > limit) {
+			listA.add(w[n]);
+			total1 = knapsackSum(w, n - 1, limit, list);
+		}
+		else {
+			listA.clear();
+			listB.clear();
+			total1 = 0;
+		}
+		if (w[n] <= limit) {
+			listB.add(w[i]);
+			total2 = knapsackSum(w, n - 1, limit - w[n], list) + w[n];
+		}
+		else {
+			listA.clear();
+			listB.clear();
+			total2 = 0;
+		}
+
 		if (max(total1, total2) == total1) {
 			list.addAll(listA);
 		}
 		else if (max(total1, total2) == total2){
 			list.addAll(listB);
 		}
-		
+
 		return max(total1, total2);
-	}
-	
-	
-//	//IN CLASS WAY
-//	public static int knapsackSum(int[] w, int n, int limit) {
-//		int total1 = 0;
-//		int total2 = 0;
-//		int i = 0;
-//		n = w.length - 1;
-//		while (i < n) {
-//			if (n < 0 || limit == 0) {
-//				return 0;
-//			}
-//			if (w[i] > limit) {
-//				i++;
-//				total1 = knapsackSum(w, n, limit - w[i]) + w[i];
-//			}
-//			else {
-//				total1 = 0;
-//			}
-//			if (w[i] < limit) {
-//				i++;
-//				total2 = knapsackSum(w, n, limit - w[i + 1]) + w[i + 1];
-//			}
-//			else {
-//				total2 = 0;
-//			}
-//			//i++;
-//		}
-//		
-//		return max(total1, total2);
-//	}
-	
+	}	
 	
 	public static void main(String[] args) {
 		
@@ -121,6 +85,7 @@ public class Knapsack {
 		Scanner testScanner;
 		try {
 			
+			//Makes array of file names from the file "tests" which lists filenames
 			testScanner = new Scanner(tests);
 			ArrayList<String> testFiles = new ArrayList<String>();
 			while(testScanner.hasNextLine()) {
@@ -129,7 +94,9 @@ public class Knapsack {
 			
 			for (String s : testFiles) {
 				try {
-					//creates file for each line in tests
+					//Makes array of all numbers in a file
+					//first number is limit
+					//rest of numbers are the melons
 					File file = new File(s);
 					Scanner inputScanner = new Scanner(file);
 					ArrayList<Integer> nums = new ArrayList<Integer>(); //list of all numbers in test files
@@ -138,21 +105,24 @@ public class Knapsack {
 						nums.add(Integer.parseInt(inputScanner.nextLine()));
 					}
 					
-					int[] w = new int[nums.size()];
+					//sets w, limit, and n
+					int[] w = new int[nums.size() - 1];
 					int limit = nums.get(0);
+					int n = w.length - 1;
+					ArrayList<Integer> list = new ArrayList<Integer>();
 					
-					System.out.print(file.getName() + "	" + limit + "	" + w);
+					//fills w
 					for (int i = 1; i < nums.size(); i++) {
-						w[i] = nums.get(i);
-						//System.out.print(nums.get(i) + ", ");
-					}
-					System.out.println();
+						w[i - 1] = nums.get(i);
+						//System.out.print(w[i] + ", ");
+					}					
 					
 					//-------------------------------------------------------------------------
-					//int[] w = {28, 3, 4, 6, 1};
-					int n = w.length - 1;
-					//limit = 40;
-					list = new ArrayList<Integer>();
+					
+					//filename, limit, and melons(w)
+					System.out.println(file.getName() + "	" + limit + "	" + Arrays.toString(w));
+					
+					//knapsack solution
 					System.out.println(Integer.toString(knapsackSum(w, n, limit, list)));
 					System.out.println(list);
 					
