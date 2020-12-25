3b5f4dab501562d95908f678647791e481ac41b4
Jack Ambery <john.ambery@bishopireton.org>
2020-12-24 22:58:14 -0500

    output writing

--- Knapsack/src/Knapsack.java
+++ Knapsack/src/Knapsack.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,6 +8,13 @@ import java.util.Scanner;
 
 public class Knapsack {
 
+	/**
+	 * Returns the greater of two given integers.
+	 * 
+	 * @param i first integer to be compared
+	 * @param k second integer to be compared
+	 * @return the larger of the two given integers
+	 */
 	public static int max(int i, int k) {
 		if (i > k) {
 			return i;
@@ -14,9 +22,15 @@ public class Knapsack {
 		return k;
 	}
 	
-	//w is all melons
-	//n is number of melons allowed
-	//limit is max weight allowed
+	/**
+	 * Performs the knapsack problem. Finds the weight of the best combination of given watermelon
+	 * weights that does not exceed the knapsack weight limit.
+	 * 
+	 * @param w the array of weights of the watermelons
+	 * @param n the number of watermelons allowed to be used, the total number of watermelons
+	 * @param limit the weight limit of the knapsack, combined weight of watermelons can not exceed this number
+	 * @return the total weight of watermelons used in the 
+	 */
 	public static int knapsackSum(int[] w, int n, int limit) {
 		int total1 = 0;
 		int total2 = 0;
@@ -38,9 +52,17 @@ public class Knapsack {
 		return max(total1, total2);
 	}
 	
-	
-//	
-	//	
+	/**
+	 * Performs the knapsack problem. Finds the weight of the best combination of given watermelon
+	 * weights that does not exceed the knapsack weight limit. Also keeps track of the watermelons
+	 * used in the final, best combination.
+	 * 
+	 * @param w the array of weights of the watermelons
+	 * @param n the number of watermelons allowed to be used, the total number of watermelons
+	 * @param limit the weight limit of the knapsack, combined weight of watermelons can not exceed this number
+	 * @param list an ArrayList of the weights used in the best combination
+	 * @return the total weight of watermelons used in the 
+	 */
 	public static int knapsackSum(int[] w, int n, int limit, ArrayList<Integer> list) {
 		int total1 = 0;
 		int total2 = 0;
@@ -79,6 +101,13 @@ public class Knapsack {
 		return max(total1, total2);
 	}	
 	
+	/**
+	 * Main method for Knapsack class. Takes in "tests" file which indicates certain test
+	 * files. For each of those files, the knapsackSum method is applied, and the
+	 * results are printed to the "knapsack.txt" file
+	 * 
+	 * @param args 
+	 */
 	public static void main(String[] args) {
 		
 		File tests = new File("tests");
@@ -91,6 +120,10 @@ public class Knapsack {
 			while(testScanner.hasNextLine()) {
 				testFiles.add(testScanner.nextLine());
 			}
+			
+			//Output writer
+			File output = new File("knapsack.txt");
+			PrintWriter p = new PrintWriter(output);
 			
 			for (String s : testFiles) {
 				try {
@@ -117,26 +150,41 @@ public class Knapsack {
 						//System.out.print(w[i] + ", ");
 					}					
 					
-					//-------------------------------------------------------------------------
+					
+					//printing
 					
 					//filename, limit, and melons(w)
-					System.out.println(file.getName() + "	" + limit + "	" + Arrays.toString(w));
+					p.println(file.getName() + "	" + limit + "	" + Arrays.toString(w));
+					p.println();
 					
-					//knapsack solution
-					System.out.println(Integer.toString(knapsackSum(w, n, limit, list)));
-					System.out.println(list);
+					//computes knapsack problem
+					knapsackSum(w, n, limit, list);
+					
+					//prints used watermelons, if none are used, prints "No possible watermelons"
+					if (!list.isEmpty()) {
+						for (int i : list) {
+							p.println(i + " pound watermelon");
+						}
+					}
+					else {
+						p.println("No possible watermelons");
+					}
+					p.println();
 					
 					inputScanner.close();
 				}
+				
 				catch (FileNotFoundException e) { 
-					System.out.println("Invalid File");
+					p.println("**ERROR**");
+					p.println("The file: \"" + s + "\" was not found.");
 					
 				}
 			}
 			testScanner.close();
+			p.close();
 		} 
 		catch (FileNotFoundException e) {
-			System.out.println("Invalid File");
+			System.out.println("The file containing tests was not found.");
 		}
 
 	}
