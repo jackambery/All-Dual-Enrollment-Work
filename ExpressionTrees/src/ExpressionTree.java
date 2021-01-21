3f2bb77d008d87859c06c7069e5288f1d190560a
Jack Ambery <john.ambery@bishopireton.org>
2021-01-20 21:44:48 -0500

    all working perfectly :)

--- ExpressionTrees/src/ExpressionTree.java
+++ ExpressionTrees/src/ExpressionTree.java
@@ -2,50 +2,74 @@ import java.util.Stack;
 
 public class ExpressionTree extends TreeNode implements Expressions {
 
-	//constructors
+	/**
+	 * This constructor creates a new ExpressionTree by calling the super TreeNode class and sets the root, left node, and right
+	 * node all with given values.
+	 * 
+	 * @param initValue value given for root
+	 * @param initLeft value given for left node
+	 * @param initRight value given for right node
+	 */
 	public ExpressionTree(Object initValue, TreeNode initLeft, TreeNode initRight) {
 		super(initValue, initLeft, initRight);
 	}
 
+	/**
+	 * This constructor creates a new ExpressionTree by calling the super TreeNode class and sets the root but sets
+	 * left and right nodes to null.
+	 * 
+	 * @param initValue value given for root
+	 */
 	public ExpressionTree(Object initValue) {
 		super(initValue);
 	}
-	
-	//methods
+
+	/**
+	 * This method takes in an array of strings which represent the terms of
+	 * a post fix expression then builds an expression tree with the terms.
+	 * 
+	 * @param exp array of expression terms (integers, "+", and "*")
+	 * @return an expression tree consisting of every element in exp
+	 */
 	public static ExpressionTree buildTree(String[] exp) {
 		Stack<ExpressionTree> stack = new Stack<ExpressionTree>();
 		for (int i = 0; i < exp.length; i++) {
-			
-			if (exp[i] != "+" && exp[i] != "*") {
+
+			if (!exp[i].equals("+") && !exp[i].equals("*")) {
 				stack.push(new ExpressionTree(exp[i]));
 			}
-			else if (exp[i] == "+") {
+			else if (exp[i].equals("+")) {
 				ExpressionTree tree = new ExpressionTree(exp[i]);
 				tree.setRight(stack.pop());
 				tree.setLeft(stack.pop());
-				
+
 				stack.push(tree);
 			}
-			else if (exp[i] == "*") {
+			else if (exp[i].equals("*")) {
 				ExpressionTree tree = new ExpressionTree(exp[i]);
 				tree.setRight(stack.pop());
 				tree.setLeft(stack.pop());
-				
+
 				stack.push(tree);
 			}
 		}
 		return stack.pop();
-		
+
 	}
 
+	/**
+	 * This method calculates the value of an ExpressionTree
+	 * 
+	 * @return int integer value of ExpressionTree
+	 */
 	//@Override
 	public int evalTree() {
 		if (this.getValue() != null) {
 			if (this.getLeft() == null && this.getRight() == null) {
 				return Integer.parseInt((String) this.getValue());
 			}
-			int left = new ExpressionTree(this.getLeft().getValue()).evalTree();
-			int right = new ExpressionTree(this.getRight().getValue()).evalTree();
+			int left = ((ExpressionTree) this.getLeft()).evalTree();
+			int right = ((ExpressionTree) this.getRight()).evalTree();
 
 			if (this.getValue().equals("+")) {
 				return left + right;
@@ -57,74 +81,93 @@ public class ExpressionTree extends TreeNode implements Expressions {
 		return 0;
 	}
 
+	/**
+	 * This method, used on an ExpressionTree, returns the prefix traversal of the tree
+	 * 
+	 * @return String which is the ExpressionTree in prefix notation 
+	 */
 	@Override
 	public String toPrefixNotation() {
 		String notation = "";
-		
+
 		notation += this.getValue();
+		
 		if (this.getLeft() != null) {
-			notation += new ExpressionTree(this.getLeft().getValue()).toPrefixNotation();
+			notation += ((ExpressionTree) this.getLeft()).toPrefixNotation();
 		}
+		
 		if (this.getRight() != null) {
-			notation += new ExpressionTree(this.getRight().getValue()).toPrefixNotation();
+			notation += ((ExpressionTree) this.getRight()).toPrefixNotation();
 		}
 		return notation;
 	}
 
+	/**
+	 * This method, used on an ExpressionTree, returns the infix traversal of the tree
+	 * 
+	 * @return String which is the ExpressionTree in infix notation 
+	 */
 	@Override
 	public String toInfixNotation() {
 		String notation = "";
-		
+
 		if (this.getLeft() != null) {
-			notation += "( ";
-			notation += new ExpressionTree(this.getLeft().getValue()).toPrefixNotation();
+			notation += "(";
+			notation += ((ExpressionTree) this.getLeft()).toInfixNotation();
 		}
+		
 		notation += this.getValue();
+		
 		if (this.getRight() != null) {
-			notation += new ExpressionTree(this.getRight().getValue()).toPrefixNotation();
-			notation += " )";
+			notation += ((ExpressionTree) this.getRight()).toInfixNotation();
+			notation += ")";
 		}
 		return notation;
 	}
 
+	/**
+	 * This method, used on an ExpressionTree, returns the postfix traversal of the tree
+	 * 
+	 * @return String which is the ExpressionTree in postfix notation 
+	 */
 	@Override
 	public String toPostfixNotation() {
 		String notation = "";
-		
+
 		if (this.getLeft() != null) {
-			notation += new ExpressionTree(this.getLeft().getValue()).toPostfixNotation();
+			notation += ((ExpressionTree) this.getLeft()).toPostfixNotation();
 		}
+		
 		if (this.getRight() != null) {
-			notation += new ExpressionTree(this.getRight().getValue()).toPostfixNotation();
+			notation += ((ExpressionTree) this.getRight()).toPostfixNotation();
 		}
+		
 		notation += this.getValue();
 		return notation;
 	}
-	
+
 	/**
 	 * This method takes in a postfix expression in the form of a String array and 
 	 * returns the solution to the expression.
 	 * 
 	 * @param exp Array of strings representing an expression in postfix notation
 	 * @return the integer answer of the given expression
-	 * WORKS
 	 */
 	public static int postfixEval(String[] exp) {
 		Stack<Object> temp = new Stack<Object>();
 		for (int i = 0; i < exp.length; i++) {
-			if (exp[i] != "+" && exp[i] != "*") {
+			if (!exp[i].equals("+") && !exp[i].equals("*")) {
 				temp.push(exp[i]);
 			}
-			else if (exp[i] == "+") {
+			else if (exp[i].equals("+")) {
 				int sum = Integer.parseInt((String) temp.pop()) + Integer.parseInt((String) temp.pop());
 				temp.push(String.valueOf(sum));
 			}
-			else if (exp[i] == "*") {
+			else if (exp[i].equals("*")) {
 				int product = Integer.parseInt((String) temp.pop()) * Integer.parseInt((String) temp.pop());
 				temp.push(String.valueOf(product));
 			}
 		}
 		return Integer.parseInt((String) temp.pop());
 	}
-
 }
--- ExpressionTrees/src/ExpressionsTest.java
+++ ExpressionTrees/src/ExpressionsTest.java
@@ -2,99 +2,61 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ExpressionsTest {
 
-	//private static String[] exp = {"3", "10", "5", "2", "*", "+", "+"};
-	//private static ExpressionTree tree = new ExpressionTree(null);
-	
-public static void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in); 
-		System.out.println("What is the name of the test file?")
+		System.out.println("What is the name of the test file?");
 		String testFileName = kb.nextLine();
 		File testFile = new File(testFileName);
 		Scanner testScanner;
 		try {
 			//Makes array of file names from the file "tests" which lists filenames
 			testScanner = new Scanner(testFile);
-			
+
 			ArrayList<String> expressions = new ArrayList<String>();
 			while(testScanner.hasNextLine()) {
 				expressions.add(testScanner.nextLine());
 			}
-			
+
 			//Output writer
-			File output = new File("myAnswers.txt");
+			File output = new File("jackAmbery_Expressions.txt");
 			PrintWriter p = new PrintWriter(output);
-			
-			for (String s : testFiles) {
-				try {
-					//Makes array of all numbers in a file
-					//first number is limit
-					//rest of numbers are the melons
-					File file = new File(s);
-					Scanner inputScanner = new Scanner(file);
-					ArrayList<Integer> nums = new ArrayList<Integer>(); //list of all numbers in test files
-					
-					while(inputScanner.hasNextLine()) {
-						nums.add(Integer.parseInt(inputScanner.nextLine()));
-					}
-					if (nums.isEmpty()) {
-						p.println("No limit or weights given in \"" + s + "\".");
-						break;
-					}
-					
-					//sets w, limit, and n
-					int[] w = new int[nums.size() - 1];
-					int limit = nums.get(0);
-					int n = w.length - 1;
-					ArrayList<Integer> list = new ArrayList<Integer>();
-					
-					//fills w
-					for (int i = 1; i < nums.size(); i++) {
-						w[i - 1] = nums.get(i);
-						//System.out.print(w[i] + ", ");
-					}					
-					
-					
-					//printing
-					
-					//filename, limit, and melons(w)
-					p.println(file.getName() + "	" + limit + "	" + Arrays.toString(w));
-					p.println();
-					
-					//computes knapsack problem
-					knapsackSum(w, n, limit, list);
-					
-					//prints used watermelons, if none are used, prints "No possible watermelons"
-					if (!list.isEmpty()) {
-						for (int i : list) {
-							p.println(i + " pound watermelon");
-						}
-					}
-					else {
-						p.println("No possible watermelons");
-					}
-					p.println();
-					
-					inputScanner.close();
-				}
+
+			for (String s : expressions) {
+
+				String[] exp = s.split(" ");
+
+				ExpressionTree tree = ExpressionTree.buildTree(exp);
+
+				p.printf("Expression: " + s + "\n\n");
 				
-				catch (FileNotFoundException e) { 
-					p.println("**ERROR**");
-					p.println("The file: \"" + s + "\" was not found.");
-					
-				}
+				p.printf("Tree Value (evalTree): " + tree.evalTree() + "\n\n");
+
+				String prefix = tree.toPrefixNotation();
+				String infix = tree.toInfixNotation();
+				String postfix = tree.toPostfixNotation();
+
+				p.printf("Prefix notation: " + prefix + "\n");
+				p.printf("Infix notation: " + infix + "\n");
+				p.printf("Postfix notation: " + postfix + "\n\n");					
+
+				p.printf("Postfix String Array Evaluation (postFixEval): " + ExpressionTree.postfixEval(exp) + "\n");
+
+				p.printf("\n\n");
+
 			}
+
 			testScanner.close();
 			p.close();
 		} 
 		catch (FileNotFoundException e) {
-			System.out.println("The file entered was not found.");
-			System.out.println("Please enter a file containing tests.");
+			System.out.printf("The file entered was not found.\n");
+			System.out.printf("Please enter a file containing tests.\n");
 		}
 
+		kb.close();
 	}
 }
