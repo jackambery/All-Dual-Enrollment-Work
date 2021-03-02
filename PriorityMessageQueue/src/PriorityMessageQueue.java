c8796a0493fa6228c7dc3c6dcc1f9f8b69911467
Jack Ambery <john.ambery@bishopireton.org>
2021-03-01 21:59:06 -0500

    javadocs done

--- PriorityMessageQueue/src/PriorityMessageQueue.java
+++ PriorityMessageQueue/src/PriorityMessageQueue.java
@@ -3,16 +3,26 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * Class to construct and manipulate a PriorityMessageQueue
+ * Class to construct and manipulate a PriorityMessageQueue.
  * 
  * @author Jack Ambery
  *
  */
 public class PriorityMessageQueue {
 	
+	/**
+	 * Fields for each priority queue to be put in the inbox.
+	 */
 	private Queue<Message> q0, q1, q2, q3, q4;
+	
+	/**
+	 * Field to hold the message in 5 different priority queues.
+	 */
 	private ArrayList<Queue<Message>> inbox;
 
+	/**
+	 * Constructor sets all the queues and inbox. Adds the queues to the inbox.
+	 */
 	public PriorityMessageQueue() {
 		q0 = new LinkedList<Message>();
 		q1 = new LinkedList<Message>();
@@ -27,11 +37,22 @@ public class PriorityMessageQueue {
 		inbox.add(q4);
 	}
 
+	/**
+	 * Takes a message, looks at its priority, and adds it the the inbox in the
+	 * proper queue. 
+	 * 
+	 * @param m Message to be added to inbox.
+	 */
 	public void processMessage(Message m) {
 		int priority = m.getPriority();
 		inbox.get(priority).add(m);
 	}
 
+	/**
+	 * Method used more so to test to make sure messages were being added to inbox
+	 * correctly. Not used in main methods/code.
+	 * 
+	 */
 	public void printInbox() {
 		ArrayList<Queue<Message>> inbox2 = inbox;
 		for (int i = 0; i < inbox2.size(); i++) {
@@ -48,7 +69,7 @@ public class PriorityMessageQueue {
 	}
 	
 	/**
-	 * Gets the first message in the highest priority queue that is not empty
+	 * Gets the first message in the highest priority queue that is not empty.
 	 * 
 	 * @return the message in the highest non-empty priority queue
 	 */
@@ -63,7 +84,7 @@ public class PriorityMessageQueue {
 	}
 	
 	/**
-	 * Removes the first message in the highest, non-empty priority queue
+	 * Removes the first message in the highest, non-empty priority queue.
 	 * 
 	 */
 	public void removeMostImportant() {
