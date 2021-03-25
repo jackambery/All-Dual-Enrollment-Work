5861269858077e2bb30fe49e77907fecacb012cb
Jack Ambery <john.ambery@bishopireton.org>
2021-03-24 20:33:06 -0400

    board setup done (forgot to commit a few times)

--- 8Queens/src/ChessSquarePanel.java
+++ 8Queens/src/ChessSquarePanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -7,16 +8,25 @@ public class ChessSquarePanel extends JPanel {
 
 	private static final long serialVersionUID = -2968661381187484787L;
 	private Color color;
-	private Boolean empty;
+	private Boolean flag;
 	private final String QUEEN = "\u265B";
 	
 	public ChessSquarePanel(Color c, Boolean e) {
 		color = c;
-		empty = e;
-		
-		if (empty == false) {
-			setText(QUEEN);
-		}
+		flag = e;
 	}	
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		this.setBackground(color);
+		g.setColor(Color.yellow);
+		
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 100));	//100 is font size
+		
+		if (flag == true) {
+			g.drawString(QUEEN, 11, 95); //11 and 95 were just what looked best
+		}
+	}
 	
 }
--- 8Queens/src/Chessboard.java
+++ 8Queens/src/Chessboard.java
@@ -1,7 +1,15 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Chessboard {
-	Board board;
+
+	private ChessSquarePanel[][] tiles;
+	private static final int ROWS = 8;
+	private static final int COLS = 8;
+	private JPanel boardPanel;
 
 	/**
 	 * Default constructor sets up a new window and adds a Board to the window
@@ -10,15 +18,33 @@ public class Chessboard {
 	public Chessboard() {
 
 		JFrame window = new JFrame("Chessboard");
-		setSize(800, 800);
+		window.setSize(1000, 1000);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		board = new Board();
-		add(board);
+		boardPanel = new JPanel();
+		boardPanel.setBackground(Color.white);
+		boardPanel.setLayout(new GridLayout(ROWS, COLS));
 
-		setTitle("Game Board");
-		pack();
-		setVisible(true);
+		tiles = new ChessSquarePanel[ROWS][COLS];
+
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLS; j++) {
+
+				if ((i + j) % 2 == 0) {
+					ChessSquarePanel temp = new ChessSquarePanel(Color.white, false);
+					tiles[i][j] = temp;
+					boardPanel.add(tiles[i][j]);
+				} else {
+					ChessSquarePanel temp = new ChessSquarePanel(Color.black, false);
+					tiles[i][j] = temp;
+					boardPanel.add(tiles[i][j]);
+				}
+				tiles[i][j].setSize(100, 100);
+			}
+		}
+		window.add(boardPanel);
+
+		window.setVisible(true);
 	}
 
 	/**
@@ -28,8 +54,7 @@ public class Chessboard {
 	 */
 	public static void main(String[] args) {
 		Chessboard c = new Chessboard();
-		c.board.addPieces();
-		c.pack();
+
 	}
 
 }
